b9f64c9f2e28eec32b2eff9dac19d4504bfa27ec
Serhiy Slipchuk <slipchuks@ukr.net>
2023-05-01 03:23:30 +0300

    step 01 completed

---
+++ src/components/Feedback/Feedback.jsx
@@ -0,0 +1,68 @@
+import React from 'react'
+import css from './Feedback.module.scss'
+import { Component } from 'react'
+
+class Feedback extends Component {
+
+    constructor() {
+        super();
+    
+        this.state = {
+            good: 0,
+            neutral: 0,
+            bad: 0
+        };
+      }
+
+    addGoodFeedback = () => {
+        this.setState(prevState => {
+            return (
+                {good: prevState.good + 1}
+                )
+            }
+        )
+    }
+        
+    addNeutralFeedback = () => {
+        this.setState(prevState => {
+            return (
+                {neutral: prevState.neutral + 1}
+                )
+            }
+        )
+    }
+
+    addBadfeedback = () => {
+        this.setState(prevState => {
+            return (
+                {bad: prevState.bad + 1}
+                )
+            }
+        )
+    }
+    
+    render = () => {
+        const { good, neutral, bad } = this.state;
+        return (
+            <div className={css.feedback}>
+            <h1 className={css.pageTitle}>Caffee "Expresso"</h1>
+            <h2 className={css.title}>Please leave feedback</h2>
+            <div className={css.buttonThumb}>
+                <button className={css.button} type="button" onClick={this.addGoodFeedback} >Good</button>
+                <button className={css.button} type="button" onClick={this.addNeutralFeedback}>Neutral</button>
+                <button className={css.button} type="button" onClick={this.addBadfeedback} >Bad</button>
+            </div>
+            <h2 className={css.title}>Statistics</h2>
+            <div className={css.statThumb}>
+                <p className={css.message}>Good: {good}</p>
+                <p className={css.message}>Neutral: {neutral}</p>
+                <p className={css.message}>Bad: {bad}</p>
+
+            </div>
+        </div>
+        )
+        
+    }
+}
+
+export default Feedback
